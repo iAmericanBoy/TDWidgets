3189c7fe019ccf23f54610c34566027a6af75003
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2020-11-05 18:47:48 -0500

    Initial Commit

---
+++ TDWidgets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TDWidgets
+//
+//  Created by Dominic Lanzillotta on 11/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
