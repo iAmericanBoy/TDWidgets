0eb1a3cfe15a4fd36302f7296d144f085920419d
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2020-12-27 15:35:47 -0500

    Adds Logging

--- TDWidgets/URLSession.swift
+++ TDWidgets/URLSession.swift
@@ -7,6 +7,7 @@
 
 import Combine
 import Foundation
+import os
 
 protocol NetworkSession: AnyObject {
     func publisher(for urlRequest: URLRequest, token: Token?) -> AnyPublisher<Data, Error>
@@ -15,18 +16,22 @@ protocol NetworkSession: AnyObject {
 extension URLSession: NetworkSession {
     func publisher(for urlRequest: URLRequest, token: Token?) -> AnyPublisher<Data, Error> {
         var request = urlRequest
+        let logger = Logger(subsystem: self.description, category: "NetworkSessionPublisher")
         if let token = token {
-            request.setValue("Bearer \(token.accessToken)", forHTTPHeaderField: "Authentication")
+            request.setValue("Bearer \(token.accessToken)", forHTTPHeaderField: "Authorization")
         }
+        logger.debug("Verb: \(request.httpMethod ?? "", privacy: .public) URL: \(request.url?.absoluteString ?? "", privacy: .public) Headers: \(String(describing: request.allHTTPHeaderFields), privacy: .public)")
+
         return dataTaskPublisher(for: request)
             .tryMap { element -> Data in
                 guard let httpResponse = element.response as? HTTPURLResponse else {
-                        throw URLError(.badServerResponse)
-                    }
+                    throw URLError(.badServerResponse)
+                }
+                logger.debug("StatusCode: \(httpResponse.statusCode, privacy: .public)")
                 if httpResponse.statusCode < 200 || httpResponse.statusCode > 299 {
                     throw URLError(URLError.Code(rawValue: httpResponse.statusCode))
                 }
-                
+
                 return element.data
             }
             .eraseToAnyPublisher()
