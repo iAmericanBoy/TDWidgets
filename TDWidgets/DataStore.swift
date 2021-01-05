f6ac64b6519e418434e4a5322f816cf2d80f8733
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2021-01-05 07:53:35 -0500

    Adds MarketHours and getAccounts DataStore

--- TDWidgets/DataStore.swift
+++ TDWidgets/DataStore.swift
@@ -5,9 +5,68 @@
 //  Created by Dominic Lanzillotta on 12/30/20.
 //
 
+import Combine
 import Foundation
-
 
 // set up network calls
 // decode to datamodel
 
+protocol DataStore {
+    func getAccouts() -> AnyPublisher<AccountDataModel, Error>
+    func getMarketHours(for date: String) -> AnyPublisher<MarketDataModel, Error>
+}
+
+class DataStoreImpl: DataStore {
+    private let session: NetworkSession
+    private let authenticator: Authenticator
+
+    init(session: NetworkSession = URLSession.shared) {
+        self.session = session
+        self.authenticator = Authenticator(session: session)
+    }
+
+    func getAccouts() -> AnyPublisher<AccountDataModel, Error> {
+        var components = URLComponents(string: "https://api.tdameritrade.com/v1/accounts")
+        components?.queryItems = [URLQueryItem(name: "fields", value: "positions")]
+        guard let url = components?.url else {
+            return Fail(error: RepositoryError.urlComponents)
+                .eraseToAnyPublisher()
+        }
+
+        return performAuthenticatedRequest(URLRequest(url: url))
+    }
+
+    func getMarketHours(for date: String) -> AnyPublisher<MarketDataModel, Error> {
+        var components = URLComponents(string: "https://api.tdameritrade.com/v1/marketdata/hours")
+        components?.queryItems = [URLQueryItem(name: "markets", value: "EQUITY"),
+                                  URLQueryItem(name: "date", value: date)]
+        guard let url = components?.url else {
+            return Fail(error: RepositoryError.urlComponents)
+                .eraseToAnyPublisher()
+        }
+
+        return performAuthenticatedRequest(URLRequest(url: url))
+    }
+
+    private func performAuthenticatedRequest<T: Decodable>(_ request: URLRequest) -> AnyPublisher<T, Error> {
+        return authenticator.validAccessToken()
+            .flatMap { accessToken in
+                // we can now use this token to authenticate the request
+                self.session.publisher(for: request, accessToken: accessToken)
+            }
+            .tryCatch { error -> AnyPublisher<Data, Error> in
+                guard let serviceError = error as? URLError, serviceError.code.rawValue == 401 else {
+                    throw error
+                }
+
+                return self.authenticator.validAccessToken(forceRefresh: true)
+                    .flatMap { accessToken in
+                        // we can now use this new token to authenticate the second attempt at making this request
+                        self.session.publisher(for: request, accessToken: accessToken)
+                    }
+                    .eraseToAnyPublisher()
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
---
+++ TDWidgets/Model/DataModel/MarketDataModel.swift
@@ -0,0 +1,45 @@
+//
+//  MarketDataModel.swift
+//  TDWidgets
+//
+//  Created by Dominic Lanzillotta on 1/5/21.
+//
+
+import Foundation
+
+// MARK: MarketDataModel
+
+struct MarketDataModel: Codable {
+    let equity: EquityDataModel
+}
+
+// MARK: EquityDataModel
+
+struct EquityDataModel: Codable {
+    let eq: EqDataModel
+
+    enum CodingKeys: String, CodingKey {
+        case eq = "EQ"
+    }
+}
+
+// MARK: EqDataModel
+
+struct EqDataModel: Codable {
+    let date, marketType, exchange, category: String
+    let product, productName: String
+    let isOpen: Bool
+    let sessionHours: SessionHoursDataModel
+}
+
+// MARK: SessionHoursDataModel
+
+struct SessionHoursDataModel: Codable {
+    let preMarket, regularMarket, postMarket: [MarketHoursDataModel]
+}
+
+// MARK: MarketHoursDataModel
+
+struct MarketHoursDataModel: Codable {
+    let start, end: Date
+}
