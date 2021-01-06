31291f3fc4a8d73427cade9a348965192f1fe979
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2021-01-06 07:36:36 -0500

    Adds MarketHours Domain Model

---
+++ TDWidgets/Model/MarketHours.swift
@@ -0,0 +1,38 @@
+//
+//  MarketHours.swift
+//  TDWidgets
+//
+//  Created by Dominic Lanzillotta on 1/5/21.
+//
+
+import Foundation
+
+enum MarketType: String, Equatable {
+    case equity
+    case option
+    case future
+    case bond
+    case forex
+    case unknown
+}
+
+struct MarketHours {
+    let marketType: MarketType
+    let preMarket: (start: Date, end: Date)
+    let regularMarket: (start: Date, end: Date)
+    let postMarket: (start: Date, end: Date)
+
+    internal init(marketType: MarketType, preMarket: (start: Date, end: Date), regularMarket: (start: Date, end: Date), postMarket: (start: Date, end: Date)) {
+        self.marketType = marketType
+        self.preMarket = preMarket
+        self.regularMarket = regularMarket
+        self.postMarket = postMarket
+    }
+
+    init(_ dataModel: MarketDataModel) {
+        self.marketType = MarketType(rawValue: dataModel.equity.eq.marketType) ?? .unknown
+        self.preMarket = (start: dataModel.equity.eq.sessionHours.preMarket[0].start, end: dataModel.equity.eq.sessionHours.preMarket[0].end)
+        self.regularMarket = (start: dataModel.equity.eq.sessionHours.regularMarket[0].start, end: dataModel.equity.eq.sessionHours.regularMarket[0].end)
+        self.postMarket = (start: dataModel.equity.eq.sessionHours.postMarket[0].start, end: dataModel.equity.eq.sessionHours.postMarket[0].end)
+    }
+}
