72a0a9329b8e14ff8ea3f382e9f0d8747906b6b4
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2021-02-14 13:40:12 -0500

    Moves View files into folder

---
+++ MyWidget/Views/AccountWidgetHeaderView.swift
@@ -0,0 +1,47 @@
+//
+//  AccountWidgetHeaderView.swift
+//  MyWidgetExtension
+//
+//  Created by Dominic Lanzillotta on 2/14/21.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct AccountWidgetHeaderView: View {
+    @State var entry: Provider.Entry
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(spacing: 2) {
+                Image(systemName: entry.dayProfitLossImage)
+                    .frame(width: 25, height: 10, alignment: .center)
+                Text(entry.dayProfitLossPercentage)
+            }
+            .font(.title)
+            .foregroundColor(entry.dayProfitLossColor.opacity(0.9))
+            if entry.isSessionTypeClosed {
+                Text("\(entry.dateText)")
+                    .font(.footnote)
+                    .bold()
+                    .foregroundColor(Color.gray.opacity(0.5))
+                Divider()
+            } else {
+                Text("\(entry.date, style: .relative) \(entry.dateText)")
+                    .font(.footnote)
+                    .bold()
+                    .foregroundColor(Color.gray.opacity(0.5))
+                Divider()
+                    .frame(height: 3)
+                    .background(entry.sessionTypeColor)
+            }
+        }
+    }
+}
+
+struct AccountWidgetHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountWidgetHeaderView(entry: AccountEntry.TestingVariation.complete)
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
--- MyWidget/Views/AccountWidgetView.swift
+++ MyWidget/Views/AccountWidgetView.swift
@@ -9,45 +9,17 @@ import SwiftUI
 import WidgetKit
 
 struct AccountWidgetView: View {
-    var entry: Provider.Entry
+    @State var entry: Provider.Entry
 
     var body: some View {
         VStack(alignment: .leading) {
-            HStack(spacing: 2) {
-                Image(systemName: entry.dayProfitLossImage)
-                    .frame(width: 25, height: 10, alignment: .center)
-                Text(entry.dayProfitLossPercentage)
-            }
-            .font(.title)
-            .foregroundColor(entry.dayProfitLossColor.opacity(0.9))
-            if entry.isSessionTypeClosed {
-                Text("\(entry.dateText)")
-                    .font(.footnote)
-                    .bold()
-                    .foregroundColor(Color.gray.opacity(0.5))
-            } else {
-                Text("\(entry.date, style: .relative) \(entry.dateText)")
-                    .font(.footnote)
-                    .bold()
-                    .foregroundColor(Color.gray.opacity(0.5))
-            }
-            if entry.isSessionTypeClosed {
-                Divider()
-                    .padding(.vertical, 4)
-            } else {
-                Divider()
-                    .frame(height: 3)
-                    .background(entry.sessionTypeColor)
-                    .padding(.vertical, 4)
-            }
-
+            AccountWidgetHeaderView(entry: entry)
             if entry.row1.isEmpty == false {
                 LazyHGrid(rows: [GridItem(.flexible())], content: {
                     GridStockView(gridValue: entry.row1[0])
                     GridStockView(gridValue: entry.row1[1])
                 })
                 Divider()
-                    .padding(.vertical, 4)
             }
             if entry.row2.isEmpty == false {
                 LazyHGrid(rows: [GridItem(.flexible())], content: {
@@ -55,7 +27,6 @@ struct AccountWidgetView: View {
                     GridStockView(gridValue: entry.row2[1])
                 })
                 Divider()
-                    .padding(.vertical, 4)
             }
         }
         .padding(10)
